283b0288a14c116a9da5583810d87291c947f1ae
Thom Nichols <thom.nichols@voltserver.com>
2016-04-01 16:01:05 -0400

    refactored config handling

--- main.py
+++ main.py
@@ -10,7 +10,16 @@ from tornado import ioloop, httpclient
 
 import sysfs
 
+__all__ = ('DEFAULT_CONFIG_LOCATION', 'DEFAULT_CONFIG_STUB',
+           'main','run','io_callback','write_config')
+
+
 DEFAULT_CONFIG_LOCATION='config.ini'
+DEFAULT_CONFIG_STUB= {
+                'post_url': 'http://ops.voltserver.dev/policy_input/state',
+                'this_host': 'ioboard0',
+                'enabled_pins': '1 2 3',
+            }
 
 if __name__ == '__main__':
     logging.basicConfig(
@@ -19,30 +28,42 @@ if __name__ == '__main__':
 
 logger = logging.getLogger(__name__)
 
-
 def io_callback(config, pin, value):
-    Logger.debug('PIN[%d] %d', pin.number, int(value) )
+    '''
+    Send an HTTP POST to C{config['post_url']} with JSON body in the form:
+    {
+        host: 'ioboard0', // name identifying this device, from config['this_host']
+        input_id: 'd0',   // d1, d2, etc.
+        value: 1          // changed value
+    }
+    '''
+    logger.debug('PIN[%d] %d', pin.number, int(value) )
 
     post_body = json.dumps({
-            'id': config['id'],
-            'pin': pin.number,
+            'host': config['this_host'],
+            'input_id': 'd%d' % pin.number, # d0, d1, etc. TODO analog input support
             'value': value,
         })
 
     # POST data to the server
     http = httpclient.AsyncHTTPClient()
-    http.fetch(config['url'],
+    http.fetch(config['post_url'],
                callback=partial(handle_http_response, config, pin),
                headers={
                    'content-type':'application/json',
+                   'accept':'application/json',
                },
                method='POST',
                body=post_body )
 
-    logger.debug('HTTP [%s] >> %s', config['url'], post_body)
+    logger.debug('HTTP >> [%s] %s', config['post_url'], post_body)
 
 
 def handle_http_response(config, pin, response):
+    if response.error:
+        logger.warn('HTTP << %d %s', response.code, response.error)
+        return
+
     logger.debug('HTTP << %d %s', response.code, response.body)
 
 
@@ -54,18 +75,29 @@ def read_config( location=DEFAULT_CONFIG_LOCATION ):
 
 
 def write_default_config( location=DEFAULT_CONFIG_LOCATION ):
+    '''
+    Write a default config file to C{DEFAULT_CONFIG_LOCATION}.
+    Default config will look like:
+
+    [main]
+    post_url: http://foo.com/policy_input/state
+    this_host: ioboard0
+    enabled_pins: 1 2 3
+
+    See C{DEFAULT_CONFIG_STUB}
+    '''
     config = configparser.ConfigParser()
-    config['main'] = {
-                'url': 'http://ops.voltserver.dev/ioboard-http',
-                'id': 'http-ioboard0',
-                'enabled_pins': '1 2 3'
-            }
+    config['main'] = DEFAULT_CONFIG_STUB
 
     with open(location,'w') as file:
         config.write(file)
 
 
 def main(args):
+    '''
+    Parse cmd line opts, read config & call C{run()}
+    '''
+
     if args.write_config:
         print('Writing default config file to %s' % args.config)
         write_default_config(args.config)
@@ -73,7 +105,7 @@ def main(args):
 
     # else parse config and run main loop:
     config = read_config(args.config)
-    run(config['main'])
+    run(config['main'])  # TODO option to select alternate config section?
 
 
 def run(config):
@@ -102,7 +134,7 @@ def run(config):
 
 parser = argparse.ArgumentParser(description='SysFS GPIO poller/ HTTP forwarder')
 parser.add_argument('-c', '--config', default=DEFAULT_CONFIG_LOCATION,
-        help='Config file to use' )
+        help='Config file to use (absolute path or relative to working dir)' )
 parser.add_argument('--write-config', action='store_const', const=True,
         help='write a config file stub and exit')
 
